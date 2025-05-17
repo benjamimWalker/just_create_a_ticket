da25cb28728145c90c11ddec9d16bab6d80d01c6
benjamim <bhaskdelta2@gmail.com>
2025-05-16 23:02:35 -0300

    Add API routing

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\TicketController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::apiResource('tickets', TicketController::class)->only(['index', 'create']);
+
+
+Route::prefix('replies')->group(function () {
+});
